ad6ba5b7a05e08e579b009a06aa0de72a80b6f1f
iamindranil <indranilchakraborty692@gmail.com>
2023-06-27 16:10:44 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Exchanges from "./components/Exchanges";
 import CoinDetalis from "./components/CoinDetalis";
 import Coins from "./components/Coins";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 function App() {
   return (
     <Router>
@@ -13,8 +14,8 @@ function App() {
         <Route path="/coins" element={<Coins />}/>
         <Route path="/exchanges" element={<Exchanges/>}/>
         <Route path="/coins/:id" element={<CoinDetalis/>}/>
-
       </Routes>
+      <Footer />
     </Router>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import { Box, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"}>
+      <motion.div
+        style={{
+          height: "60vh",
+        }}
+        animate={{
+          translateY: "20px",
+        }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          repeatType: "reverse",
+        }}
+      >
+        <Image
+          w={"full"}
+          h={"full"}
+          objectFit={"contain"}
+          src="https://images.unsplash.com/photo-1639454276085-9f55d2db6570?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNyeXB0b2N1cnJlbmN5JTIwc21hbGwlMjBwaWN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+          filter={"grayscale(1)"}
+        />
+      </motion.div>
 
-export default Home
+      <Text
+        fontSize={"6xl"}
+        textAlign={"bottom"}
+        fontWeight={"thin"}
+        color={"whiteAlpha.700"}
+        mt={"-20"}
+      >
+        πCrypto
+      </Text>
+    </Box>
+  );
+};
+
+export default Home;
